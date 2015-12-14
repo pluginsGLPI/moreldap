366cea6591f55acb7069dbb83d9443452a6d0303
dethegeek <dethegeek@gmail.com>
2015-12-14 11:57:57 +0100

    Code cleanup

--- hook.php
+++ hook.php
@@ -63,103 +63,6 @@ function plugin_moreldap_uninstall() {
    plugin_moreldap_DatabaseUninstall();
 }
 
-/**
- * Hook to add more fields from LDAP
- *
- * @param $fields   array
- *
- * @return un tableau
- **/
-function plugin_retrieve_more_field_from_ldap_moreldap($fields) {
-   $pluginAuthLDAP = new PluginMoreldapAuthLDAP();
-
-   // There is no way to know which LDAP will be used, so we have
-   // to retrieve all LDAP attributes in any LDAP server
-   $result = $pluginAuthLDAP->find("");
-
-   if (is_array($result)) {
-      foreach ($result as $attribute) {
-         // Explode multiple attributes for location hierarchy
-         $locationHierarchy = explode('>', $attribute['location']);
-         foreach ( $locationHierarchy as $locationSubAttribute ) {
-            $locationSubAttribute = trim($locationSubAttribute);
-            $fields[$locationSubAttribute] = $locationSubAttribute;
-         }
-      }
-   }
-   return $fields;
-}
-
-/**
- * Hook to add more data from ldap
- *
- * @param $datas   array
- *
- * @return un tableau
- **/
-function plugin_retrieve_more_data_from_ldap_moreldap(array $fields) {
-   $user = new User();
-   if (!$user->getFromDBbyDn($fields['user_dn'])) {
-      return $fields;
-   }
-   $pluginAuthLDAP = new PluginMoreldapAuthLDAP();
-   $authLDAP = new AuthLDAP();
-
-   // default : store locations outside of any entity
-   $entityID = -1;
-
-   if ($pluginAuthLDAP->getFromDBByQuery("WHERE `id`='" . $user->fields["auths_id"] . "'")) {
-
-      $entityID = $pluginAuthLDAP->fields['entities_id'];
-
-      $locationHierarchy = explode('>', $pluginAuthLDAP->fields['location']);
-      $locationPath = array();
-      $incompleteLocation = false;
-      foreach ( $locationHierarchy as $locationSubAttribute ) {
-         $locationSubAttribute = trim($locationSubAttribute);
-         if (isset($fields['_ldap_result'][0][strtolower($locationSubAttribute)][0])) {
-            $locationPath[] = $fields['_ldap_result'][0][strtolower($locationSubAttribute)][0];
-         } else {
-            $incompleteLocation = true;
-         }
-      }
-
-      if ($incompleteLocation == false) {
-         if ($pluginAuthLDAP->fields['location_enabled'] == 'Y') {
-            $location = new Location();
-            $locationAncestor = 0;
-            $locationCompleteName = array();
-            foreach ( $locationPath as $locationItem ) {
-               $locationCompleteName[] = $locationItem;
-               $locationItem = Toolbox::addslashes_deep(array(
-                     'entities_id' => $entityID,
-                     'name' => $locationItem,
-                     'locations_id' => $locationAncestor,
-                     'completename' => implode(' > ', $locationCompleteName),
-                     'is_recursive' => $pluginAuthLDAP->fields['is_recursive'],
-                     'comment' => __("Created by MoreLDAP", "moreldap")
-               ));
-               $locationAncestor = $location->findID($locationItem);
-               if ($locationAncestor == -1) {
-                  // The location does not exists yet
-                  $locationAncestor = $location->add($locationItem);
-               }
-               if ($locationAncestor == false) {
-                  // If a location could not be imported, then give up importing children items
-                  break;
-               }
-            }
-            if ($locationAncestor != false) {
-               $fields['locations_id'] = $locationAncestor;
-            }
-         } else {
-            // If the location retrieval is disabled, enablig this line will erase the location for the user.
-            // $fields['locations_id'] = 0;
-         }
-      }
-   }
-   return $fields;
-}
 
 function plugin_moreldap_item_add_or_update_user($user) {
    Toolbox::logDebug($user);
@@ -180,20 +83,15 @@ function plugin_moreldap_item_add_or_update_user($user) {
       foreach ($locationHierarchy as $locationSubAttribute) {
          $locationSubAttribute = trim($locationSubAttribute);
          if (strlen($locationSubAttribute) > 0) {
-            //$fields[$locationSubAttribute] = $locationSubAttribute;
             $fields[] = $locationSubAttribute;
          }
       }
 
       // LDAP query to read the needed attributes for the user
-      //$field           = $pluginAuthLDAP->fields['location'];
       $ldap_connection = isset($user->input["_ldap_conn"]) ? $user->input["_ldap_conn"] : $user->fields["_ldap_conn"];
       $userdn          = isset($user->input["user_dn"]) ? $user->input["user_dn"] : $user->fields["user_dn"];
-      //$sr              = @ldap_read($ldap_connection, $userdn, "objectClass=*", array($field));
       $sr              = @ldap_read($ldap_connection, $userdn, "objectClass=*", $fields);
       $v               = AuthLDAP::get_entries_clean($ldap_connection, $sr);
-      //$locations_name  = $v[0][$field][0];
-      //$locations_name  = 'Formation > GLPI';
 
       //Find all locations needed to create the deepest one
       $locationPath = array();
@@ -254,32 +152,6 @@ function plugin_moreldap_item_add_or_update_user($user) {
          // $fields['locations_id'] = 0;
       }
    }
-
-//    $location_id = 0;
-
-//    //check if this location exist
-//    $location  = new location;
-//    $locations = $location->find("name = '$locations_name'");
-//    if (count($locations) > 0) {
-//       //get existing location
-//       $first_location = array_shift($locations);
-//       $locations_id   = $first_location['id'];
-
-//    } else {
-//       //create new location
-//       $new_location = array(
-//          'name'         => $locations_name,
-//          'comment'      => __("Created by MoreLDAP", "moreldap"),
-//          'entities_id'  => 0,
-//          'is_recursive' => 1
-//       );
-//       $locations_id = $location->add($new_location);
-//    }
-
-//    $user->update(array(
-//       'id'           => $user->getID(),
-//       'locations_id' => $locations_id,
-//    ));
 }
 
 /**
